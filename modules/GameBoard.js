3f56f2216aafa3d9b85139826cdcd3f4128f58ea
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-15 01:42:13 +0500

    Create GameBoard factory to replicate an 8x8 board

---
+++ modules/GameBoard.js
@@ -0,0 +1,14 @@
+const GameBoard = function () {
+  let possibleMoves = [];
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      possibleMoves.push([i, j]);
+    }
+  }
+
+  return {
+    possibleMoves,
+  };
+};
+
+export { GameBoard }
